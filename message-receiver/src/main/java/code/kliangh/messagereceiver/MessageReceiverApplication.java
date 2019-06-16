bce9081d82550ac2fc1bc37b25ae6960e2318b2a
kliangh <kunliang.hou@gmail.com>
2019-06-16 10:22:44 +0800

    spring-boot-message-example. 1. added three node to project

---
+++ message-receiver/src/main/java/code/kliangh/messagereceiver/MessageReceiverApplication.java
@@ -0,0 +1,13 @@
+package code.kliangh.messagereceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageReceiverApplication.class, args);
+    }
+
+}
---
+++ worker-node/src/main/java/code/kliangh/workernode/WorkerNodeApplication.java
@@ -0,0 +1,13 @@
+package code.kliangh.workernode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerNodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerNodeApplication.class, args);
+    }
+
+}
