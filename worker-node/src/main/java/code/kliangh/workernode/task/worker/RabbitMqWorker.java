7c91159eb10177c06c9ea52b21b3b7ea789d5d34
kliangh <kunliang.hou@gmail.com>
2019-09-30 12:30:20 +0800

    spring-boot-message-example. 1. implemented rabbit mq worker node

--- worker-node/src/main/java/code/kliangh/workernode/task/worker/RabbitMqWorker.java
+++ worker-node/src/main/java/code/kliangh/workernode/task/worker/RabbitMqWorker.java
@@ -1,4 +1,20 @@
 package code.kliangh.workernode.task.worker;
 
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.text.MessageFormat;
+
+@Component
+@RabbitListener(queues = "Rabbit")
 public class RabbitMqWorker {
+
+    @RabbitHandler
+    public void receive(String message) {
+        StringBuilder messageBuilder = new StringBuilder(message);
+
+        System.out.println(MessageFormat.format(" [x] Message sent: {0}",
+                                                messageBuilder.toString()));
+    }
 }
